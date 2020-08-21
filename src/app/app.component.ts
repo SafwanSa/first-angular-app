278317c4a163c3a73105e746e34178a9d156a4e7
Safwan <safwan9f@gmail.com>
2020-08-21 03:41:47 +0300

    style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = 'map'
+  viewMode = 'map';
   title = 'first-angular-app';
 }
