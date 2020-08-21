44926c7457e2798554ff8adbb3a91c97208d0755
Safwan <safwan9f@gmail.com>
2020-08-21 03:37:48 +0300

    Seperate the  nav from the contenet and place it in thr header using app c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  viewMode = 'map'
   title = 'first-angular-app';
 }
--- src/app/test-switch/test-switch.component.ts
+++ src/app/test-switch/test-switch.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test-switch',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestSwitchComponent implements OnInit {
 
-  viewMode = 'map';
+  @Input() viewMode: string;
 
   constructor() { }
 
