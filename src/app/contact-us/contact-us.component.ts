e4045aedbcc6a3f6d11b9b5dc7121efd0dbbf27a
Safwan <safwan9f@gmail.com>
2020-08-22 05:56:25 +0300

    Add the ngForm and ngModelGroup

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -3,17 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   log(x) {
     console.log(x);
   }
 
+  submit(f) {
+    console.log(f);
+    // f.valid ...
+  }
 }
